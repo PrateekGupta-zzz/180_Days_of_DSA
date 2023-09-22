c0f2bc7c1528b74896f4533425b21d1742fd3ad3
Prateek Gupta <63515419+PrateekGupta-zzz@users.noreply.github.com>
2023-09-23 04:34:49 +0530

    Create even_till_n.cpp
    prints even numbers from 1 to n

---
+++ Day_4/even_till_n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+    
+    for(int i=1; i<=num; i+=1){
+        if(i%2==0){
+            cout << i<< endl;
+        };
+    };
+};
