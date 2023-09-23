1144e696c0e7b344057bd8a02e1ade522e97c30a
Prateek Gupta <63515419+PrateekGupta-zzz@users.noreply.github.com>
2023-09-23 11:16:14 +0530

    Create greater_lesser.cpp
    determines whether first_number is greater or the second

---
+++ Day_4/greater_lesser.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    
+    if(a>b){
+        cout << "first_number is greater than second_number";
+    }
+    else{
+        cout << "second_number is greater than first_number";
+    }
+}
