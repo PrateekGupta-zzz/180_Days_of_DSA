ad085911d9917f0c15c771c71c297931793a9eda
Prateek Gupta <63515419+PrateekGupta-zzz@users.noreply.github.com>
2023-09-23 11:48:18 +0530

    prints odd number upto n

---
+++ Day_4/odd_till_n.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Write C++ code here
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int i=1;i<=num;i+=2){
+        cout << i<<endl;
+    }
+}
